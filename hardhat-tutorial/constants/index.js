8f41e5501abd5cce304123f58cf677ee04033a2a
PraiseHack <praise.johnny@yahoo.com>
2022-11-18 10:24:16 +0100

    first commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xcc00dF6e3724A6d6Bd04018E51bE50B025Bf7525";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-dapp-eight.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
